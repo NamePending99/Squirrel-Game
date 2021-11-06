a3c3bff0dcb30b072dc69561fe90650b0d55c43c
Aasmund Nørsett <aasmunno@stud.ntnu.no>
2021-11-06 14:46:08 +0100

    add google spreadsheet api

--- frontend/client.js
+++ frontend/client.js
@@ -1,5 +1,13 @@
-var socket = io.connect("localhost:3333", {secure: true});
+var socket = io.connect("192.168.2.159:3333", {secure: true});
 
 socket.on("clientConnected", function(id) {
     console.log("Connected to server with id: " + id);
 });
+
+socket.on("updateData", function(data) {
+    var test_content = document.getElementById("page-content");
+
+    test_content.innerHTML = `
+        ${data[0]} ${data[1]}
+    `
+})
